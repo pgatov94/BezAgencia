45d742de69a0c58a99882a0dd068ba14ef46d8ec
pgatov94 <pgatov94@gmail.com>
2026-07-08 11:14:16 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://ototukzpkeyvndlodffm.supabase.co";
-const SUPABASE_ANON_KEY = sb_publishable_CkKK9ze_fpweahPn54juuQ_X02FWTUt
+const SUPABASE_ANON_KEY = "sb_publishable_CkKK9ze_fpweahPn54juuQ_X02FWTUt";
 
 export const isSupabaseConfigured =
   !SUPABASE_URL.includes("ТВОЯ-ПРОЕКТ") && !SUPABASE_ANON_KEY.includes("ТВОЯ-ANON-PUBLIC-KEY");
