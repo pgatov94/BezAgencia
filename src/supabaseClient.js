606fa501ca20aa7528fdf4fd31110398ce1ae5c8
pgatov94 <pgatov94@gmail.com>
2026-07-08 11:05:29 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://ototukzpkeyvndlodffm.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90b3R1a3pwa2V5dm5kbG9kZmZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0OTYwMzgsImV4cCI6MjA5OTA3MjAzOH0.X0QDofSnzkI1Lr3yXkqQMptWnwb81EdeEbSbaDhKEzk";
+const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90b3R1a3pwa2V5dm5kbG9kZmZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0Nzg3ODMsImV4cCI6MjA5OTA1NDc4M30.X0QDofSnzkIlLr3yXkqQMptWnwb8lEdeEbSbaDhKEzk
 
 export const isSupabaseConfigured =
   !SUPABASE_URL.includes("ТВОЯ-ПРОЕКТ") && !SUPABASE_ANON_KEY.includes("ТВОЯ-ANON-PUBLIC-KEY");
